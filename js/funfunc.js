3f4df6df11e975506e0d5947980639f1f9f32649
ParthKharwar <parthjk98@gmail.com>
2020-01-12 11:23:00 +0530

    landing page test

--- js/funfunc.js
+++ js/funfunc.js
@@ -1,10 +1,18 @@
 var char = 0;
-var consoleText = 'Test';
-var typeSpeed = 75;
+var consoleText = 'This is a test sentence\nThis is a second test sentence';
+var typeSpeed = 100;
+var loadSymbols = ['/','-','\\','|']
+var loadSymCnt = 0
 
 function typeWriter() {
   if (char < consoleText.length) {
     document.getElementById("console").innerHTML += consoleText.charAt(char++);
     setTimeout(typeWriter, typeSpeed);
   }
+}
+
+function loadAnimation() {
+  document.getElementById("loading").innerHTML = loadSymbols[loadSymCnt++];
+	loadSymCnt = loadSymCnt % 4;
+  setTimeout(loadAnimation, typeSpeed);
 }
